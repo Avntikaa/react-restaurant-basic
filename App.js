b5c835a5bae715359c59b0e128a92c0f0153dbd8
Avantika <99116667+Avntikaa@users.noreply.github.com>
2023-03-17 13:04:00 +0530

    headermodalsummary

---
+++ App.js
@@ -0,0 +1,13 @@
+import Header from "./components/layout/Header";
+import HeaderModal from "./components/Meals/HeadeSummary";
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <HeaderModal/>
+    </div>
+  );
+}
+
+export default App;
---
+++ HeadeSummary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HeaderSummary.css'
+const HeaderModal = () => {
+  return (
+    <div className='headersummary'>
+        <h2>Delicious Food,Delivered to you</h2>
+        <p>Choose you favourite meal from our board selection of availabel meals and enjoy a
+             delicious lunch or dinner at home</p>
+    <p>All our meals are cooked with high-quality ingredients, just-in-time and
+        of course by experienced chefs!</p>
+
+    </div>
+  )
+}
+
+export default HeaderModal
