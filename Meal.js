be0d3a6243c769f91eb56e16a7d2c85dd4c18d0a
Avantika <99116667+Avntikaa@users.noreply.github.com>
2023-03-17 14:00:18 +0530

    buttonamount

--- Meal.js
+++ Meal.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { FaPlus } from 'react-icons/fa'
 import './Meal.css'
 
 const Meal = (props) => {
   return (
     <div className='eachmeal'>
-        <h3>{props.meal.dish}</h3>
+      <span> 
+         <h3>{props.meal.dish}</h3>
         <h5>{props.meal.desc}</h5>
         <h4>${props.meal.price}</h4>
+        </span>
+        <span className='addinttocart'>
+          <div className='amount'>
+            <h4>Amount</h4>
+            <h4 className='quantity'>1</h4>
+          </div>
+          <button className='addbtn'>
+             <span id='icon'><FaPlus/></span>
+          <span>Add</span>
+          </button>
+        </span>
+      
+
     </div>
   )
 }
